0bc55d9489561c6a19a282494a0a36da6ef21355
karan <karan.rokhale@gmail.com>
2013-09-20 12:42:57 +0530

    deleted scaffold files.
    Signed-off-by: karan <karan.rokhale@gmail.com>

--- app/models/job.rb
+++
@@ -1,7 +0,0 @@
-class Job < ActiveRecord::Base
-  attr_accessible :contractor, :customer, :employee_id, :employer_id, :shift, :week_of, :contractor_lname, :rate_of_pay
-  belongs_to  :employee
-  belongs_to  :employer
-
-
-end
